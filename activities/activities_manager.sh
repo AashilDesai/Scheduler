cc1bfa0759e49990b136b4371ba20cf70d01bc26
AashilDesai <aashild@seas.upenn.edu>
2016-12-04 18:14:44 -0500

    Planned out interface

--- activities/activities_manager.sh
+++ activities/activities_manager.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
-#This is be a very small thing, but it will allow users to add and list classes.
+# the user will be able to (1) ask for the tasks for today, (2) store a task on a day, 
+#(3) remove a task for a day, (4) put in study mode, (5) enable desktop notifications, (6) ask for what to do now
 
-filename="classes.txt"
+#the following commands will work:
+#task [date]
+#tasks [date]
+#get [date]
+#instead of "[date]", "today", "yesterday", and "tomorrow" will be acceptable (as well as "t" "y" and blank 
+	#(for "tomorrow", "yesterday" and "today" respectiley))
 
 
-#first see if the file even exists
-if [! -f $filename ]
-	then
-	>$filename
-fi
+#For storing a task on a day:
+#set [date] [time] [task]
+	#date formats: "t", "tomorrow", "today", Month/Day/Year (day can be 2 digits or 1, month same, year 2 or 4)
+	#time can be 24 hour or 12-our (followed by pm or am (caps irrelevant))
+	#if no date, assumed for today
+	#if invalid time or task is blank, will throw error
 
-if [[ $1 == "add" ]] 
-	then
-	if grep -q $2 "$filename"; then
-		echo "Class already exists"
-	else
-		then
-		echo $2 >> $filename
-		echo "Class added!"
-	fi
-elif [[ $1 == "list" ]]
-	then
-	echo "Classes: "
-	cat $filename | echo
-else
-	then
-	echo "Command not recogmized"
-	return 1
-fi
+#for removing, same except "remove [date] [time] [task]"
+
+#for studymode, "study", "studymode" and "study" will all work
+
+#for desktop notification, "desktop on", "desktop off", and "desktop" all work (last toggles)
+
+#for ask what to do "tasks", workds (defaults to [tasks today])
+
